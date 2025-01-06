11079f3e96720dedd2a0f18bea58a675c21f38fc
SOLN4R <soln4r@gmail.com>
2025-01-06 23:48:12 +0300

    Mini-game: Guess the number

--- main.lua
+++ main.lua
@@ -1,17 +1,70 @@
--- io.read()
+-- Mini-game: Guess the number
 
-local player_name
-local player_level
+math.randomseed(os.time())
+local MIN_NUMBER = 1
+local MAX_NUMBER = 100
 
-io.write("Enter a name: ")
-player_name = io.read()
-
-io.write("Enter the level: ")
-player_level = io.read()
-
-
-local function greetPlayer(name, level)
-    return "Hello, " .. name .. "! Your level is " .. level .. "."
+local function greetMessage()
+    print("Welcome to the Guess the Number game!")
+    print("Rules: The program guesses a number in the range from " .. MIN_NUMBER .. " to " .. MAX_NUMBER .. ",")
+    print("and you need to guess it. Good luck!")
 end
 
-print(greetPlayer(player_name, player_level))
+local function inputGuess()
+    while true do
+        local input
+        io.write("\nEnter a guess: ")
+        input = io.read()
+        input = tonumber(input)
+        if input and math.floor(input) == input and input >= MIN_NUMBER and input <= MAX_NUMBER then
+            return input
+        else
+            print("Enter an integer from " .. MIN_NUMBER .. " to " .. MAX_NUMBER .. ".")
+        end
+    end
+end
+
+local function repeatGame()
+    local input
+    while true do
+        io.write("\nDo you want to play again? (yes/no): ")
+        input = io.read():lower()
+        if input == "y" or input == "yes" then
+            return true
+        elseif input == "n" or input == "no" then
+            return false
+        else
+            print("Enter \"yes\" or \"no\".")
+        end
+    end
+end
+
+local function gameLoop()
+    local attempts = 0
+    local secretNumber = math.random(MIN_NUMBER, MAX_NUMBER)
+    print("\nThe program guessed a number, and the game began.")
+
+    while true do
+        local guess = inputGuess()
+        attempts = attempts + 1
+        -- Checking the guess
+        if guess < secretNumber then
+            print("The secret number is higher.")
+
+        elseif guess > secretNumber then
+            print("The secret number is lower.")
+        else
+            print("Congratulations, you guessed it! Attempts: " .. attempts)
+            break
+        end
+    end
+end
+
+-- Main
+greetMessage()
+while true do
+    gameLoop()
+    if not repeatGame() then
+        break
+    end
+end
