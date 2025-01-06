e19b53e7bb3d8f2be2dfec08b64dab5368ae6ee2
SOLN4R <soln4r@gmail.com>
2025-01-07 00:22:57 +0300

    Tables

--- main.lua
+++ main.lua
@@ -1,70 +1,41 @@
--- Mini-game: Guess the number
+-- Tables
 
-math.randomseed(os.time())
-local MIN_NUMBER = 1
-local MAX_NUMBER = 100
+local players = {}
 
-local function greetMessage()
-    print("Welcome to the Guess the Number game!")
-    print("Rules: The program guesses a number in the range from " .. MIN_NUMBER .. " to " .. MAX_NUMBER .. ",")
-    print("and you need to guess it. Good luck!")
+local function addPlayer(tbl, name, level, score)
+    table.insert(tbl, {name = name, level = level, score = score})
 end
 
-local function inputGuess()
-    while true do
-        local input
-        io.write("\nEnter a guess: ")
-        input = io.read()
-        input = tonumber(input)
-        if input and math.floor(input) == input and input >= MIN_NUMBER and input <= MAX_NUMBER then
-            return input
-        else
-            print("Enter an integer from " .. MIN_NUMBER .. " to " .. MAX_NUMBER .. ".")
-        end
+local function printPlayers(tbl)
+    for _, value in ipairs(tbl) do
+        print("Player: " .. value.name .. ", Level: " .. value.level .. ", Score: " .. value.score)
     end
 end
 
-local function repeatGame()
-    local input
-    while true do
-        io.write("\nDo you want to play again? (yes/no): ")
-        input = io.read():lower()
-        if input == "y" or input == "yes" then
-            return true
-        elseif input == "n" or input == "no" then
-            return false
-        else
-            print("Enter \"yes\" or \"no\".")
+local function getTopPlayer(tbl)
+    local name
+    local highestScore = 0
+    for _, value in ipairs(tbl) do
+        if value.score > highestScore then
+            name = value.name
+            highestScore = value.score
         end
     end
-end
-
-local function gameLoop()
-    local attempts = 0
-    local secretNumber = math.random(MIN_NUMBER, MAX_NUMBER)
-    print("\nThe program guessed a number, and the game began.")
-
-    while true do
-        local guess = inputGuess()
-        attempts = attempts + 1
-        -- Checking the guess
-        if guess < secretNumber then
-            print("The secret number is higher.")
-
-        elseif guess > secretNumber then
-            print("The secret number is lower.")
-        else
-            print("Congratulations, you guessed it! Attempts: " .. attempts)
-            break
-        end
+    if highestScore == 0 then
+        return -1
     end
+    return name
 end
 
 -- Main
-greetMessage()
-while true do
-    gameLoop()
-    if not repeatGame() then
-        break
-    end
+addPlayer(players, "John Kido", 9, 180)
+addPlayer(players, "Jack McCallister", 12, 240)
+addPlayer(players, "Alan Lonesel", 4, 96)
+
+printPlayers(players)
+
+if getTopPlayer(players) == -1 then
+    print("The best player is missing.")
+else
+    print("The player with the most points: " .. getTopPlayer(players))
 end
