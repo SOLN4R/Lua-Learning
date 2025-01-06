307a3d771b10e218c71cc174e449ba9875dc1cd0
SOLN4R <soln4r@gmail.com>
2025-01-06 15:15:16 +0300

    Functions

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
--- Cycle for
+-- Functions
 
-local players = {"Jack", "John", "Alan"}
+local name = "Jack McCallister"
+local level = 1
 
-for i, value in ipairs(players) do
-    print("Player " .. i .. ": " .. value)
+local function greetPlayer(name, level)
+    return "Hello, " .. name .. "! Your level is " .. level .. "."
 end
+
+print(greetPlayer(name, level))
