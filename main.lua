81fddb25422c19070f5f80381cc8b3c27217a7b9
SOLN4R <soln4r@gmail.com>
2025-01-06 14:52:33 +0300

    Variables, data types

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
-print("Hello World!")
+-- Variables, data types
+
+local name = "Jack McCallister"
+local age = 24
+local level = "Beginner"
+
+print("Name: " .. name .. "\nAge: " .. age .. "\nLevel: " .. level)
