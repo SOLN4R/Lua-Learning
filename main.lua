c14c256b15b9905f2a26eb872eea97c35f9218b6
SOLN4R <soln4r@gmail.com>
2025-01-08 01:15:21 +0300

    Uploading images

--- main.lua
+++ main.lua
@@ -1,25 +1,16 @@
--- Basic geometric primitives
+-- Uploading images
+
+local image
+local x, y
 
 function love.load()
+    image = love.graphics.newImage("assets/character.png")
+    x, y = love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    -- line
-    love.graphics.setColor(1, 0, 0) -- red
-    love.graphics.line(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-
-    -- rectangle
-    love.graphics.setColor(0, 1, 0) -- green
-    love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - 50, love.graphics.getHeight() / 2 - 25, 100, 50)
-
-    -- circle
-    love.graphics.setColor(0, 0, 1) -- blue
-    love.graphics.circle("fill", love.graphics.getWidth() - 50, 50, 30)
-
-    -- triangle
-    love.graphics.setColor(1, 1, 0) -- yellow
-    love.graphics.polygon("fill", 50, love.graphics.getHeight() - 50, 100, love.graphics.getHeight() - 50, 75, love.graphics.getHeight() - 100)
+    love.graphics.draw(image, x - (image:getWidth() * 0.5), y - (image:getHeight() * 0.5))
 end
