d61e157682f2bd23d966d6c7aa2a3e60f33dec9d
SOLN4R <soln4r@gmail.com>
2025-01-08 02:09:53 +0300

    Animation using sprites

--- main.lua
+++ main.lua
@@ -1,16 +1,36 @@
--- Uploading images
+-- Animation using sprites
 
-local image
-local x, y
+local spriteSheet
+local quads = {}
+local currentFrame = 1
+local frameTime = 0.1
+local timer = 0
 
 function love.load()
-    image = love.graphics.newImage("assets/character.png")
-    x, y = love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5
+    spriteSheet = love.graphics.newImage("assets/walk_sprite_sheet.png")
+
+    local frameWidth, frameHeight = 96, 128
+    local sheetWidth = spriteSheet:getWidth()
+    local sheetHeight = spriteSheet:getHeight()
+
+    for y = 0, sheetHeight - frameHeight, frameHeight do
+        for x = 0, sheetWidth - frameWidth, frameWidth do
+            table.insert(quads, love.graphics.newQuad(x, y, frameWidth, frameHeight, sheetWidth, sheetHeight))
+        end
+    end
 end
 
 function love.update(dt)
+    timer = timer + dt
+    if timer >= frameTime then
+        timer = timer - frameTime
+        currentFrame = currentFrame + 1
+        if currentFrame > #quads then
+            currentFrame = 1
+        end
+    end
 end
 
 function love.draw()
-    love.graphics.draw(image, x - (image:getWidth() * 0.5), y - (image:getHeight() * 0.5))
+    love.graphics.draw(spriteSheet, quads[currentFrame], 300, 200)
 end
