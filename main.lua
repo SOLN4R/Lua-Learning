8791368c5cffeaca3583e40ea4db44c0cbe172a1
SOLN4R <soln4r@gmail.com>
2025-01-07 19:17:12 +0300

    object-oriented approach

--- main.lua
+++ main.lua
@@ -1,36 +1,96 @@
--- table.insert
+-- object-oriented approach
 
-local players = {
-    {name = "Jack", level = 9, score = 150},
-    {name = "Alice", level = 12, score = 200},
-    {name = "John", level = 2, score = 20},
-    {name = "Alan", level = 4, score = 20},
-    {name = "Kevin", level = 13, score = 300}
-}
+-- Parent Class Player
+local Player = {}
+Player.__index = Player
+
+function Player:new(name, level, score)
+    local object = {name = name or "null", level = level or 1, score = score or 0}
+    setmetatable(object, Player)
+    return object
+end
+
+function Player:increaseLevel()
+    print("\nIncreasing " .. self.name .. "'s level...")
+    self.level = self.level + 1
+    self.score = self.score + 20
+end
+
+function Player:decreaseScore(amount)
+    if amount <= 0 then
+        print("[Error] The amount of reduction must be greater than 0.")
+        return
+    end
+    self.score = math.max(0, self.score - amount)
+    print(string.format("\nDecreasing %s's score by %d...", self.name, amount))
+end
+
+function Player:resetLevel()
+    if self.level <= 1 then
+        print("[Error] The player's level is less than or equal to 1.")
+        return
+    end
+    self.level = 1
+    print("\nResetting the " .. self.name .. " level...")
+end
+
+function Player:printInfo()
+    print(string.format("Name: %s | Level: %d | Score: %d", self.name, self.level, self.score))
+end
+
+-- Child Class VipPlayer
+local VipPlayer = setmetatable({}, {__index = Player})
+VipPlayer.__index = VipPlayer
+
+function VipPlayer:new(name, level, score, score_bonus)
+    local object = Player.new(self, name, level, score)
+    setmetatable(object, VipPlayer)
+    object.name = string.format("%s (VIP)", name)
+    object.score_bonus = score_bonus or 50
+    return object
+end
+
+function VipPlayer:increaseLevel()
+    print("\nIncreasing " .. self.name .. "'s level...")
+    self.level = self.level + 1
+    self.score = self.score + self.score_bonus
+end
+
+-- Players array
+local players = {}
 
 local function printPlayers(tbl, message)
-    print("\n" .. message)
+    message = message or "\nPlayers:"
+    print(message)
+
     for _, player in ipairs(tbl) do
-        print("Player: " .. player.name .. ", Level: " .. player.level .. ", Score: " .. player.score)
+        player:printInfo()
     end
+
 end
 
-local function getBestPlayers(tbl, min_score)
-    local result = {}
-    for _, player in ipairs(tbl) do
-        if player.score >= min_score then
-            table.insert(result, {name = player.name, level = player.level, score = player.score})
-        end
-    end
-    return result
-end
+-- Main
+local player_1 = Player:new("Jack", 9, 150)
+local player_2 = VipPlayer:new("Alice", 12, 200)
+local player_3 = Player:new("Alan", 3, 30)
 
-printPlayers(players, "All players:")
+table.insert(players, player_1)
+table.insert(players, player_2)
+table.insert(players, player_3)
 
-local min_score = 100
-local best_players = getBestPlayers(players, min_score)
-if #best_players == 0 then
-    print("\nThere are no players who have scored more than " .. min_score .. " points.")
-else
-    printPlayers(best_players, "Players with scores >= " .. min_score .. ":")
-end
+printPlayers(players)
+
+player_1:decreaseScore(50)
+player_1:printInfo()
+player_1:increaseLevel()
+player_1:printInfo()
+
+player_2:increaseLevel()
+player_2:printInfo()
+player_2:decreaseScore(10)
+player_2:printInfo()
+
+player_3:resetLevel()
+player_3:printInfo()
+
+printPlayers(players)
