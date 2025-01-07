a1a3fc07c8fbbc78b1a83932336ef9dc7e879568
SOLN4R <soln4r@gmail.com>
2025-01-08 00:59:07 +0300

    Initial LÖVE

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+
+    t.version = "11.5"
+    t.console = true
+
+    t.window.title = "LÖVE Learning"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.resizable = false
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+    t.window.vsync = 1
+end
--- main.lua
+++ main.lua
@@ -1,16 +1,5 @@
--- Modules
+-- Initial LÖVE
 
-local Events = require("events")
-local Player = require("player")
-
-Events.clearLog()
-Events.printLog()
-
-local player_1 = Player:new("Jack", 10, 200)
-local player_2 = Player:new("Alice", 8, 160)
-
-player_1:increaseLevel()
-player_2:decreaseScore(20)
-player_2:resetLevel()
-
-Events.printLog()
+function love.draw()
+    love.graphics.print("Hello, Love2D!", 300, 200)
+end
